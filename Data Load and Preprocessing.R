b90ee95604dbf299719613a348d9ee815d8916f8
Jitender Munjal <Jitender.Munjal@gmail.com>
2017-03-29 02:06:41 -0600

    Submitting Exploratory Data Analysis Assignment-1

---
+++ Data Load and Preprocessing.R
@@ -0,0 +1,24 @@
+
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+        temp <- tempfile()
+        download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        file <- unzip(temp)
+        unlink(temp)
+}
+
+
+power <- read.table(file, header=T, sep=";")
+summary(power)
+
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+summary(power$Date)
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+summary(df)
+
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
